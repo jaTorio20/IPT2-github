9f9ba3ea1fb53d2e562f80d0c5dbb3f362802338
Rose Anne <roseannemalana01@gmail.com>
2024-09-10 16:30:44 +0800

    Divide

--- calculator.java
+++ calculator.java
@@ -12,4 +12,13 @@ public class calculator{
     public static int multiply(int a, int b){
         return a*b;
     }
-}
+
+    public static int divide(int a, int b){
+        if(b==0){
+            throw new ArithmeticException("Cannot divided by zero");
+        }
+            return a/b;
+        }
+
+
+    }
